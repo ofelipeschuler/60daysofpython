c646a4e9164c7a0f0ab2c295d838449506d72e6c
Felipe Schuler <fschulerrr@gmail.com>
2025-03-29 10:29:06 +0000

    Dia 4 - Adicionando Laço de Repetição

---
+++ Day4/laco_de_repeticao.py
@@ -0,0 +1,9 @@
+# usando o for para imprimir números de 0 a 10
+for numero in range(11):
+    print(numero)
+    
+# usando o while para imprimir números de 0 a 10
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1
