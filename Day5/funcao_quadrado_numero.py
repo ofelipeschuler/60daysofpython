e64dd1d80403061746ade73aa6ba506432e5b005
ofelipeschuler <fschulerrr@gmail.com>
2025-03-30 18:50:27 +0100

    Criando uma função que retorna o quadrado de um número em duas versões.

---
+++ Day5/funcao_quadrado_numero.py
@@ -0,0 +1,18 @@
+def quadrado_numero(numero):
+    """
+    Função que retorna o quadrado de um número.
+    :param numero: Número a ser elevado ao quadrado.
+    :return: O quadrado do número.
+    """
+    return numero ** 2
+  
+def quadrado_numerov2(numero):
+    """
+    Função que retorna o quadrado de um número.
+    :param numero: Número a ser elevado ao quadrado.
+    :return: O quadrado do número.
+    """
+    return numero * numero
+  
+print(quadrado_numero(5))  # Saída: 25
+print(quadrado_numerov2(5))  # Saída: 25
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 - [X] Dia 2 - Soma de dois números de duas formas diferentes
 - [X] Dia 3 - Condicional que verifica se um número é par ou ímpar
 - [X] Dia 4 - Laço de Repetição
-- [ ] Dia 5 -  
+- [ ] Dia 5 - Função que imprime o quadrado de um número 
